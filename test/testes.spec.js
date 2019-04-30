f5128e9d1936f9946187e1c9791666fefad994f6
karineSooma <karinesooma5411@gmail.com>
2019-04-30 16:18:05 -0300

    eslint sem errors

--- test/testes.spec.js
+++ test/testes.spec.js
@@ -3,19 +3,19 @@ let expect = chai.expect;
 var cpfValidatorTeste = require('../lib/index');
 
 describe('cpfValidator', () => {
-    it('string is empty', () => {
-      expect(cpfValidatorTeste.cpfValidator('')).to.be.false;
-    });
-    it('string has repeated numbers', () => {
-      expect(cpfValidatorTeste.cpfValidator('12121212121')).to.be.false;
-    });
-    it('string has more than eleven numbers', () => {
-      expect(cpfValidatorTeste.cpfValidator('123456789111')).to.be.false;
-    });
-    it('string has letters instead of numbers', () => {
-      expect(cpfValidatorTeste.cpfValidator('abcdefghijk')).to.be.false;
-    });
+  it('string is empty', () => {
+    expect(cpfValidatorTeste.cpfValidator('')).to.be.false;
   });
+  it('string has repeated numbers', () => {
+    expect(cpfValidatorTeste.cpfValidator('12121212121')).to.be.false;
+  });
+  it('string has more than eleven numbers', () => {
+    expect(cpfValidatorTeste.cpfValidator('123456789111')).to.be.false;
+  });
+  it('string has letters instead of numbers', () => {
+    expect(cpfValidatorTeste.cpfValidator('abcdefghijk')).to.be.false;
+  });
+});
 
 // let chai = require("chai");
 // let expect = chai.expect;
